ebc9ffbfc6e54677f5958a5613aee2df668b60b9
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2023-03-29 12:00:40 +0200

    Add menubar toggles (#895)

---
+++ static/js/index.js
@@ -0,0 +1,40 @@
+// script for menu toggles
+var siteMenuSubmenus = document.getElementsByClassName(
+    "td-sidebar-nav__section-title td-sidebar-nav__section with-child"
+);
+
+function submenuToggle(menu, toggle) {
+    let open = false;
+    if (toggle.children[0].className === "fas fa-chevron-right") {
+        open = true;
+    }
+
+    // toggle displayed icon
+    if (open) {
+        toggle.children[0].className = "fas fa-chevron-down";
+    } else {
+        toggle.children[0].className = "fas fa-chevron-right";
+    }
+
+    // add or remove collapse class to children
+    let immediateChildren = menu.querySelector("ul").children;
+    if (open) {
+        for (let c of immediateChildren) {
+            c.classList.remove("collapse");
+        }
+    } else {
+        for (let c of immediateChildren) {
+            c.classList.add("collapse");
+        }
+    }
+}
+
+for (let menu of siteMenuSubmenus) {
+    // add event listener on menu toggle
+    let toggle = menu.querySelector("span").querySelector(".submenu-toggle");
+    if (toggle) {
+        toggle.addEventListener("click", function () {
+            submenuToggle(menu, toggle);
+        });
+    }
+}
