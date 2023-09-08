e2dd7926cb1d59211429027d58f0ff0ca76bcb9b
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2023-09-08 14:05:09 -0400

    Update toc null error

--- assets/js/index.js
+++ assets/js/index.js
@@ -97,18 +97,21 @@ if (toc) {
 
         for (var i = 0; i < tocItems.length; i++) {
             let item = tocItems[i];
-            var targetBounds = item.target.getBoundingClientRect();
+            if (item.target) {
 
-            if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
-                visibleItems += 1;
-                item.listItem.classList.add( 'toc-active' );
-                atLeastOne = true;
-            } else {
-                item.listItem.classList.remove( 'toc-active' );
-            }
+                var targetBounds = item.target.getBoundingClientRect();
 
-            if (targetBounds.bottom < windowHeight) {
-                lastElem = item;
+                if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
+                    visibleItems += 1;
+                    item.listItem.classList.add( 'toc-active' );
+                    atLeastOne = true;
+                } else {
+                    item.listItem.classList.remove( 'toc-active' );
+                }
+
+                if (targetBounds.bottom < windowHeight) {
+                    lastElem = item;
+                }
             }
 
         }
