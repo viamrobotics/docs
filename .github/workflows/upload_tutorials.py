aa7f3ad82dfe99af53f2ff99aedc64e8323fc803
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2024-08-23 18:52:36 +0200

    DOCS-1573: Use python to upload tutorials

---
+++ .github/workflows/upload_tutorials.py
@@ -0,0 +1,56 @@
+import asyncio
+import time
+import os
+import typesense
+import json
+
+async def main():
+
+    typesense_client = typesense.Client({
+        'api_key': os.environ['TYPESENSE_API_KEY_R'],
+        'nodes': [{
+            'host': 'cgnvrk0xwyj9576lp-1.a1.typesense.net',
+            'port': '443',
+            'protocol': 'https'
+        }],
+        'connection_timeout_seconds': 2
+    })
+
+    time_now = int(time.time())
+
+    # Get tutorials from tutorials/typesense.json
+    with open('tutorials/typesense.json') as f:
+        resources = json.load(f)
+        for r in resources:
+            print("RESOURCE")
+            print(r)
+            r["last_updated"] = time_now
+            print(r)
+            insert_resp = typesense_client.collections['tutorials'].documents.upsert(r)
+            print("INSERTED")
+            print(insert_resp)
+
+    # Get quickstarts from quickstarts/typesense.json
+    with open('quickstarts/typesense.json') as f:
+        resources = json.load(f)
+        for r in resources:
+            print("RESOURCE")
+            print(r)
+            r["last_updated"] = time_now
+            print(r)
+            insert_resp = typesense_client.collections['tutorials'].documents.upsert(r)
+            print("INSERTED")
+            print(insert_resp)
+
+    # Deleting documents that didn't get updated (presumably deleted)
+    try:
+        res = typesense_client.collections['tutorials'].documents.delete({'filter_by': 'last_updated: <' + str(time_now)})
+        print("Resources deleted")
+        print(res)
+    except Exception as e:
+        print(e)
+        pass
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
