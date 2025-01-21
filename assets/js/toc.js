67c2e8ee3da90471cbd88ce10fb8e153b7d4e1c5
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2025-01-21 13:23:17 +0100

    Fix css to hide collapsed toc

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -58,6 +58,7 @@ if (toc) {
         let atLeastOne = false;
         let lastElem;
 
+        console.log(tocItems)
         for (var i = 0; i < tocItems.length; i++) {
             let item = tocItems[i];
             if (item && item.target) {
