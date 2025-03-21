caf5822ec1d65784a49c578ce8d92276d23a5d6f
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2025-03-21 10:05:35 +0100

    Revert "DOCS-3712: Remove ask ai button (#4091)"
    This reverts commit 50a3a593a284b47ad9c3fbaf5fbe9a447e20f03d.

--- assets/js/inkeep.js
+++ assets/js/inkeep.js
@@ -86,6 +86,7 @@ const inkeepWidgetAI = Inkeep().embed(config);
 
 // Add event listener to open the Inkeep modal when the button is clicked
 inkeepButtonTop.addEventListener("click", handleOpen);
+inkeepButtonBottom.addEventListener("click", handleOpen);
 
 // Create an HTML element that the Inkeep widget will be inserted into.
 const nav = document.querySelector("#navsearch");
