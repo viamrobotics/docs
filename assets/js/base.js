af89dd2f796991e92f73097d16aada429c373a3c
nathan contino <nathan.contino@viam.com>
2025-06-13 08:38:01 -0400

    Force anchor self-link icon visibility while copied notification displays

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,12 +2,15 @@ function handleAnchorClick(event, anchor) {
    // write the full anchor URL to the clipboard
    navigator.clipboard.writeText(location.href.split('#')[0] + '#' + anchor ); 
 
+   const anchorLink = document.getElementById('anchor-link-' + anchor);
    const copiedReaction = document.getElementById('copied-' + anchor);
    const copiedReactionText = document.getElementById('copied-' + anchor + '-text');
+   anchorLink.classList.add('force-visible');
    copiedReaction.classList.add('visible');
    copiedReactionText.textContent = 'Copied!';
    
    setTimeout(() => {
+     anchorLink.classList.remove('force-visible');
      copiedReaction.classList.remove('visible');
      copiedReactionText.textContent = '';
    }, 2000);
