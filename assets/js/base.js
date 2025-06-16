551f5d0cb0038d4b93375f398f7c20412e5c9120
nathan contino <nathan.contino@viam.com>
2025-06-16 10:37:34 -0400

    Hide copied element altogether to prevent padding-only visual bug

--- assets/js/base.js
+++ assets/js/base.js
@@ -7,11 +7,13 @@ function handleAnchorClick(event, anchor) {
    const copiedReactionText = document.getElementById('copied-' + anchor + '-text');
    anchorLink.classList.add('force-visible');
    copiedReaction.classList.add('visible');
+   copiedReaction.style.display = 'inline-block';
    copiedReactionText.textContent = 'Copied!';
    
    setTimeout(() => {
      anchorLink.classList.remove('force-visible');
      copiedReaction.classList.remove('visible');
+     copiedReaction.style.display = 'none';
      copiedReactionText.textContent = '';
    }, 2000);
 }
