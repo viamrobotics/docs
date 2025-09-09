43ee0b83dd6a84e405d11ddaa0c7e8158c985473
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2025-09-09 17:05:01 +0200

    Update code samples to use label

--- static/include/examples/capture-annotate-images/tag-images.py
+++ static/include/examples/capture-annotate-images/tag-images.py
@@ -62,7 +62,7 @@ async def main() -> int:
         for tag in data[0].metadata.annotations.classifications:
             await data_client.remove_tags_from_binary_data_by_ids(
                 binary_ids=[BINARY_DATA_ID],
-                tags=tag.id
+                tags=[tag.label]
             )
             print(f"Deleted tag: {tag.id}")
     # :remove-end:
